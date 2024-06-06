7985319daa00e7ec4696f68a63a0163d42cbdcf3
xieyaxin <1549469775@qq.com>
2024-06-06 14:24:07 +0800

    结合插件更新

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,21 @@ import VueEditor from '@/components/VueEditor/VueEditor.vue';
 import Vue2Editor from '@/components/Vue2Editor/Vue2Editor.vue';
 import SandPack from '@/components/SandPack/SandPack.vue';
 import codeplayer from '@/components/codeplayer/index.vue';
-
+import MdEditor from '@/components/MdEditor/MdEditor.vue'
 import { utoa, atou } from '@/utils';
-import { ref, watchEffect, toRaw, watch } from 'vue';
+import { ref, watchEffect, toRaw, watch, nextTick, onMounted } from 'vue';
 import { watchDeep } from '@vueuse/core';
+import { v4 as uuidv4 } from 'uuid';
 
 const curEditor = ref('flems');
+window.uuid 
+
 const query = new URLSearchParams(location.search);
+if (query.has('uuid')) {
+  window.uuid = query.get('uuid')
+}else {
+  window.uuid = uuidv4()
+}
 if (query.has('vue')) {
   curEditor.value = 'vue';
 }
@@ -29,11 +37,60 @@ if (query.has('sand-react-js')) {
 if (query.has('player')) {
   curEditor.value = 'player';
 }
+const key = ref(0)
+if (query.has('md')) {
+  curEditor.value = 'md';
+  key.value++
+}
 
 let isInIframe = ref(false)
 if (window.frames.length != parent.frames.length) {
   isInIframe.value = true
 } 
+
+const text = ref("")
+let init = true
+if (location.hash && query.has('md')) {
+    try {
+        const data = atou(location.hash.slice(1))
+        nextTick(() => {
+          text.value = data
+        })
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+function throttle(fn, delay) {
+  let timer = null
+  return function(...args) {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, args)
+        timer = null // 在 delay 时间后可以再次执行
+      }, delay)
+    }
+  }
+}
+const updateR = throttle(()=>chrome.runtime.sendMessage(window.ChromeExtensionID, { command: "update" }, (res)=>{
+  console.log(res)
+  if(!res.code || !res) {
+    alert("更新收藏的网址失败，请注意可能造成数据丢失")
+  }
+}), 500)
+watchEffect(() => {
+  if(window.ChromeExtensionID) {
+    updateR();
+  }
+  history.replaceState({}, '', `?md&uuid=${window.uuid}#` + utoa(text.value))
+})
+function handleChangeCode(a: string) {
+  if(!window.ChromeExtensionID && !init) {
+    alert("尚检测到插件ID，无法更新收藏的网址，请注意可能造成数据丢失")
+  }
+  init = false
+  text.value = a
+}
 </script>
 
 <template>
@@ -44,7 +101,8 @@ if (window.frames.length != parent.frames.length) {
       <a href="/?vue27" class="mr-5">vue2.7 sfc</a>
       <a href="/?vue26#eNo9T9FuwyAM/BXLT5u0gtJKe4hYtf0HL6x4K1MgCJxsUpR/n0naIgQ+22ffLfiRs5onwh5NvZSQ+WwT/eWxMHj6ctPAsNgE4B27p+ctBCjEU0l7oZ1Yv3vojqcdr+2TR67Rj6ECmGIeHJMgAOPDvAUSXrvzsrQpsK5GC7rlQ8oTw3yIo6fhzaJ0WNzZeqcb/RiKLxhiE36ILqufOiYxtUm0t0K12N9FWxTXDVu8Mufaa33xSWiyKcxFJWKdctTS9X5Ur6o7ycbKDSuqUX2W8bdSEYLFu19c/wFNam6P" class="mr-5">vue2.6 sfc</a>
       <a href="/?sand-react-js" class="mr-5">sand-react-js</a>
-      <a href="/?player">codeplayer</a>
+      <a href="/?player" class="mr-5">codeplayer</a>
+      <a href="/?md">markdown</a>
     </div>
     <template v-if="curEditor === 'vue'">
       <VueEditor class="h-[100%]"></VueEditor>
@@ -61,6 +119,9 @@ if (window.frames.length != parent.frames.length) {
     <template v-if="curEditor === 'player'">
       <codeplayer></codeplayer>
     </template>
+    <template v-if="curEditor === 'md'">
+      <MdEditor style="height:100%" :value="text" @change="(v: string) => handleChangeCode(v)"></MdEditor>
+    </template>
   </div>
 </template>
 
@@ -75,6 +136,10 @@ if (window.frames.length != parent.frames.length) {
 }
 
 .CodeMirror.cm-s-default{
+  height: 100%;
+}
+
+.bytemd{
   height: 100%;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+
+window.ChromeExtensionID = undefined
+window.addEventListener("message", function (event) {
+    if (event.source != window) {
+        return;
+    }
+    if (event.data.type === "getChromeExtensionID") {
+        window.ChromeExtensionID = event.data.id
+    }
+}, false);
